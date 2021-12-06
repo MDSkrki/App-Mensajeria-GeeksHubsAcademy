adb85042765225e0fbd018740832afc140ce2ba3
Mihai Daniel Somkereki <somkereki@hotmail.com>
2021-12-06 18:25:41 +0100

    added collapsable Sidebar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Sidebar Navigation System
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  } 
