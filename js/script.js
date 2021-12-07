363dc6205127422bc7b888f10ea0a676c0b263fa
Mihai Daniel Somkereki <somkereki@hotmail.com>
2021-12-07 10:40:29 +0100

    creacion de un div para el contenido principal (main view) y traspaso del título de la app en el sideBar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// Sidebar Navigation System
+// Sidebar Navigation Open-Collapse System
 function openNav() {
-    document.getElementById("sideNav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sideNav").style.width = "18em";
+    document.getElementById("main").style.marginLeft = "18em";
 }
 
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-  } 
+} 
