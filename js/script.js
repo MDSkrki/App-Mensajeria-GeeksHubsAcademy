8a4423dbdd441c5c378542557c701dd23218230b
Mihai Daniel Somkereki <somkereki@hotmail.com>
2021-12-08 15:55:24 +0100

    changed some id names so they all use underscore and added input bar for messages and send button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // Sidebar Navigation Open-Collapse System
 function openNav() {
-    document.getElementById("sideNav").style.width = "18em";
+    document.getElementById("side_nav").style.width = "18em";
     document.getElementById("main").style.marginLeft = "18em";
+    document.getElementById("open_btn").style.display = "none";
 }
 
 function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("side_nav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("open_btn").style.display = "inline";
 } 
